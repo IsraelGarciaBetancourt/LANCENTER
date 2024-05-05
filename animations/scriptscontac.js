b2b6cb270701042b5c0c83982b44c349a901e10e
IsraelGarciaBetancourt <60408504@continental.edu.pe>
2024-05-05 18:16:50 -0500

    VERSION 4.0
    VERSION 4.0

--- animations/scriptscontac.js
+++ animations/scriptscontac.js
@@ -1,36 +1,35 @@
 function addClass() {
-    document.body.classList.add("sent");
-  }
-  
-  sendLetter.addEventListener("click", addClass);
+  document.body.classList.add("sent");
+}
+
+sendLetter.addEventListener("click", addClass);
 
 function validarInput() {
-  var nombre = document.getElementById('nombreInput').value;
-  var email = document.getElementById('emailInput').value;
-  var telefono = document.getElementById('telefonoInput').value;
-  var mensaje = document.getElementById('mensajeInput').value;
+var nombre = document.getElementById('nombreInput').value;
+var email = document.getElementById('emailInput').value;
+var telefono = document.getElementById('telefonoInput').value;
+var mensaje = document.getElementById('mensajeInput').value;
 
-  if (nombre === '') {
-    alert('Por favor escriba su nombre.');
-    return false;
-  }
-
-  if (email === '') {
-    alert('Por favor escriba su email.');
-    return false;
-  }
-
-  if (telefono === '') {
-    alert('Por favor escriba su teléfono.');
-    return false;
-  }
-
-  if (mensaje === '') {
-    alert('Por favor escriba su mensaje.');
-    return false;
-  }
-
-  // Si todos los campos están llenos, el formulario se envía
-  return true;
+if (nombre === '') {
+  alert('Por favor escriba su nombre.');
+  return false;
 }
-  
+
+if (email === '') {
+  alert('Por favor escriba su email.');
+  return false;
+}
+
+if (telefono === '') {
+  alert('Por favor escriba su teléfono.');
+  return false;
+}
+
+if (mensaje === '') {
+  alert('Por favor escriba su mensaje.');
+  return false;
+}
+
+// Si todos los campos están llenos, el formulario se envía
+return true;
+}
